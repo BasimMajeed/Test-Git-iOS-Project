8f28b23bb1e0707444eb2169573e622d0a56c590
Basim Majeed <mbm@Muhammad-Basim-Majeed.local>
2015-05-26 15:29:27 +0500

    added a test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Basim Majeed on 26/05/2015.
+//  Copyright (c) 2015 MNTechSolutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Basim Majeed on 26/05/2015.
+//  Copyright (c) 2015 MNTechSolutions. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
